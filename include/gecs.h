07898ec2793a7d23208dfde1af495cc2d7ddf0c8
Gabriele Cocchieri <gabriele.cocchieri@gmail.com>
2026-07-22 00:11:24 +0200

    comments refactored

--- include/gecs.h
+++ include/gecs.h
@@ -47,55 +47,45 @@ typedef struct
 	uint32_t fieldCount;
 } ComponentTypeInfo;
 
+typedef uint64_t ComponentsPresence[(GECS_MAX_REGISTERED_COMPONENTS + 63) / 64 /* manual ceil */];
+
 /*
-typedef enum
-{
-    BOOL,
-    STRING,
-    FLOAT,
-    DOUBLE,
-
-    INT8,
-    INT16,
-    INT32,
-    INT64,
-
-    UINT8,
-    UINT16,
-    UINT32,
-    UINT64
-} FieldType;
-*/
-
-//@brief Initializes GECS, which is mandatory to use the library.
+ * @brief Initializes GECS, which is mandatory to use the library.
+ */
 void GECS_Init();
 
-//@brief Register a System Archetype in the system.
-//The order in which the ComponentTypeIDs are specified is crucial to the component data access in the callback function,
-//since it matches exactly with the indices.
-//@param callback The function called upon system execution, entity having that exact component set.
-//@param componentCount The count of components specified in the variadic part of the arguments.
-//The variadic field expects the ComponentTypeIDs of the System's Archetype, in order.
-//@return The SystemID used to identify the newly created System's Archetype in the system.
+/*
+ * @brief Register a System Archetype in the system.
+ * The order in which the ComponentTypeIDs are specified is crucial to the component data access in the callback function,
+ * since it matches exactly with the indices.
+ * @param callback The function called upon system execution, entity having that exact component set.
+ * @param componentCount The count of components specified in the variadic part of the arguments.
+ * The variadic field expects the ComponentTypeIDs of the System's Archetype, in order.
+ * @return The SystemID used to identify the newly created System's Archetype in the system.
+ */
 SystemID GECS_RegisterSystem(void (*callback)(ID, void**), int componentCount, ...);
 
-//@brief Execute the callback defined by the system registration.
-//@param systemID The idendificator for the system to execute.
+/*
+ * @brief Execute the callback defined by the system registration.
+ * @param systemID The idendificator for the system to execute.
+ */
 void GECS_ExecuteSystem(SystemID systemID);
 
-//@brief Register a component type in the system.
-//To attach any component to an entity, it must be registered through this function.
-//@param size The size in bytes of the singular component.
-//@param name The name of this component type.
-//@param fieldCount The number of elements (fields) this component consists of.
-//The variadic field is used to describe the elements this component has, coupled with the
-//previous argument "fieldCount", to provide introspection information for the system.
-//This field is made of "fieldType" and "fieldName" pairs, so for each field in the component,
-//insert the type and name in this order. The fields must be EXACTLY ordered and layed out as
-//you would use them in memory, with the fieldType also indicating the byte size of that field.
-//NOTE: the field types must be defined by the user, since it might have its own custom types,
-//and not basic ones such as simple primitives (int, bool, float). For that, do your own enum.
-//@return The unique id assigned to the newly registered component type.
+/*
+ * @brief Register a component type in the system.
+ * To attach any component to an entity, it must be registered through this function.
+ * @param size The size in bytes of the singular component.
+ * @param name The name of this component type.
+ * @param fieldCount The number of elements (fields) this component consists of.
+ * The variadic field is used to describe the elements this component has, coupled with the
+ * previous argument "fieldCount", to provide introspection information for the system.
+ * This field is made of "fieldType" and "fieldName" pairs, so for each field in the component,
+ * insert the type and name in this order. The fields must be EXACTLY ordered and layed out as
+ * you would use them in memory, with the fieldType also indicating the byte size of that field.
+ * NOTE: the field types must be defined by the user, since it might have its own custom types,
+ * and not basic ones such as simple primitives (int, bool, float). For that, do your own enum.
+ * @return The unique id assigned to the newly registered component type.
+ */
 ComponentTypeID GECS_RegisterComponent(size_t size, const char* name, uint32_t fieldCount, ...);
 
 /*
@@ -106,38 +96,55 @@ ComponentTypeID GECS_RegisterComponent(size_t size, const char* name, uint32_t f
  */
 const ComponentTypeInfo* GECS_GetComponentTypeInfo(ComponentTypeID componentTypeID);
 
-//@brief Creates an entity in the system.
-//More than 1 entity can have the same name at the same time.
-//@param name The name of the entity.
-//@return The newly created entity on success. GECS_INVALID_ID on failure.
+/*
+ * @brief Creates an entity in the system.
+ * More than 1 entity can have the same name at the same time.
+ * @param name The name of the entity.
+ * @return The newly created entity on success. GECS_INVALID_ID on failure.
+ */
 ID GECS_CreateEntity(const char* name);
 
-//@brief Deletes an existing entity and its associated components.
-//@param entity The target's entity ID.
+/*
+ * @brief Deletes an existing entity and its associated components.
+ * @param entity The target's entity ID.
+ */
 void GECS_DeleteEntity(ID entity);
 
-//@brief Checks if an entity exists.
-//@param entity The target's entity ID.
-//@return True if the entity exists, false otherwise.
+/*
+ * @brief Checks if an entity exists.
+ * @param entity The target's entity ID.
+ * @return True if the entity exists, false otherwise.
+ */
 bool GECS_DoesEntityExist(ID entity);
 
-//@brief Attach a registered component to an existing entity.
-//You cannot attach the same component type to the same entity more than once.
-//@param componentData An allcated buffer long as the component type's size.
+/*
+ * @brief Attach a registered component to an existing entity.
+ * You cannot attach the same component type to the same entity more than once.
+ * @param componentData An allcated buffer long as the component type's size.
+ */
 void GECS_AttachComponent(ID entity, ComponentTypeID componentTypeID, void* componentData);
-//@brief Detach a registered component from an existing entity.
+
+/*
+ * @brief Detach a registered component from an existing entity.
+ */
 void GECS_DetachComponent(ID entity, ComponentTypeID componentTypeID);
 
-//@brief Retrieves the component object from a specified existing entity.
-//@return The retrieved component data on success. NULL if the entity doesn't have the component, useful to check existence.
+/*
+ * @brief Retrieves the component object from a specified existing entity.
+ * @return The retrieved component data on success. NULL if the entity doesn't have the component, useful to check existence.
+ */
 void* GECS_GetComponent(ID entity, ComponentTypeID componentTypeID);
 
-//@brief Retrieves a specified entity's name.
-//@param entity The target entity's ID.
-//@return The entity's null terminated name.
+/*
+ * @brief Retrieves a specified entity's name.
+ * @param entity The target entity's ID.
+ * @return The entity's null terminated name.
+ */
 const char* GECS_GetEntityName(ID entity);
 
-//@brief Cleans up GECS, do it as soon as you're done with the library.
+/*
+ * @brief Cleans up GECS, do it as soon as you're done with the library.
+ */
 void GECS_CleanUp();
 
 #endif
